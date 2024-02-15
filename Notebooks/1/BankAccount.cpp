08804bf5e050b131d386b8dbac62629754b0a493
Mahdi <mahdiiporheydarii@gmail.com>
2024-02-15 21:56:48 +0330

    Structure modified

---
+++ Notebooks/1/BankAccount.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+using namespace std;
+// Class for a bank account:
+class BankAccount
+{
+public:
+    BankAccount(int dollars, int cents, double rate);
+    // Initializes the account balance to $dollars.cents and
+    // initializes the interest rate to rate percent.
+    BankAccount(int dollars, double rate);
+    // Initializes the account balance to $dollars.00 and
+    // initializes the interest rate to rate percent.
+    BankAccount();
+    // Initializes the account balance to $0.00
+    // and the interest rate to 0.0%.
+    void set(int dollars, int cents, double rate);
+    // Postcondition: The account balance has been set to $dollars.cents;
+    // The interest rate has been set to rate percent.
+    void set(int dollars, double rate);
+    // Postcondition: The account balance has been set to $dollars.00.
+    // The interest rate has been set to rate percent.
+    void update();
+    // Postcondition: One year of simple interest has been added
+    // to the account balance.
+    double getBalance();
+    // Returns the current account balance.
+    double getRate();
+    // Returns the current account interest rate as a percentage.
+    void output(ostream &outs);
+    // Precondition: If outs is a file output stream, then
+    // outs has already been connected to a file.
+    // Postcondition: Account balance and interest rate
+    // have been written to the stream outs.
+private:
+    double balance;
+    double interestRate;
+    double fraction(double percent);
+    // Converts a percentage to a fraction. For example, fraction(50.3)
+    // returns 0.503.
+};
+int main()
+{
+    BankAccount account1(100, 2.3), account2;
+    cout << "account1 initialized as follows:\n";
+    account1.output(cout);
+    cout << "account2 initialized as follows:\n";
+    account2.output(cout);
+    account1 = BankAccount(999, 99, 5.5);
+    cout << "account1 reset to the following:\n";
+    account1.output(cout);
+    return 0;
+}
+
+void BankAccount::set(int dollars, int cents, double rate)
+{
+    if ((dollars < 0) || (cents < 0) || (rate < 0))
+    {
+        cout << "Illegal values for money or interest rate.\n";
+        return;
+    }
+    balance = dollars + 0.01 * cents;
+    interestRate = rate;
+}
+
+void BankAccount::set(int dollars, double rate)
+{
+    if ((dollars < 0) || (rate < 0))
+    {
+        cout << "Illegal values for money or interest rate.\n";
+        return;
+    }
+    balance = dollars;
+    interestRate = rate;
+}
+
+void BankAccount::update()
+{
+    balance = balance + fraction(interestRate) * balance;
+}
+
+double BankAccount::fraction(double percentValue)
+{
+    return (percentValue / 100.0);
+}
+
+double BankAccount::getBalance()
+{
+    return balance;
+}
+double BankAccount::getRate()
+{
+    return interestRate;
+}
+
+// Uses iostream:
+void BankAccount::output(ostream &outs)
+{
+    outs.setf(ios::fixed);
+    outs.setf(ios::showpoint);
+    outs.precision(2);
+    outs << "Account balance $" << balance << endl;
+    outs << "Interest rate " << interestRate << "%" << endl;
+}
+
+BankAccount::BankAccount(int dollars, int cents, double rate)
+{
+    if ((dollars < 0) || (cents < 0) || (rate < 0))
+    {
+        cout << "Illegal values for money or interest rate.\n";
+        return;
+    }
+    balance = dollars + 0.01 * cents;
+    interestRate = rate;
+}
+BankAccount::BankAccount(int dollars, double rate)
+{
+    if ((dollars < 0) || (rate < 0))
+    {
+        cout << "Illegal values for money or interest rate.\n";
+        return;
+    }
+    balance = dollars;
+    interestRate = rate;
+}
+BankAccount::BankAccount() : balance(0), interestRate(0.0)
+{
+    // Body intentionally empty
+}
---
+++ Notebooks/1/Birthday.cpp
@@ -0,0 +1,78 @@
+// Program to demonstrate the class DayOfYear.
+#include <iostream>
+using namespace std;
+class DayOfYear
+{
+public:
+    void input();
+    void output();
+    void set(int newMonth, int newDay);
+    // Precondition: newMonth and newDay form a possible date.
+    // Postcondition: The date is reset according to the arguments.
+    int getMonth();
+    // Returns the month, 1 for January, 2 for February, etc.
+    int getDay();
+    // Returns the day of the month.
+private:
+    void checkDate();
+    int month;
+    int day;
+};
+int main()
+{
+    DayOfYear today, bachBirthday;
+    cout << "Enter today's date:\n";
+    today.input();
+    cout << "Today's date is ";
+    today.output();
+    bachBirthday.set(3, 21);
+    cout << "J. S. Bach's birthday is ";
+    bachBirthday.output();
+    if (today.getMonth() == bachBirthday.getMonth() &&
+        today.getDay() == bachBirthday.getDay())
+        cout << "Happy Birthday Johann Sebastian!\n";
+    else
+        cout << "Happy Unbirthday Johann Sebastian!\n";
+    return 0;
+}
+// Uses iostream:
+void DayOfYear::input()
+{
+    cout << "Enter the month as a number: ";
+    cin >> month;
+    cout << "Enter the day of the month: ";
+    cin >> day;
+    checkDate();
+}
+
+void DayOfYear::output()
+{
+    cout << "month = " << month
+         << ", day = " << day << endl;
+}
+
+void DayOfYear::set(int newMonth, int newDay)
+{
+    month = newMonth;
+    day = newDay;
+    checkDate();
+}
+
+void DayOfYear::checkDate()
+{
+    if ((month < 1) || (month > 12) || (day < 1) || (day > 31))
+    {
+        cout << "Illegal date. Aborting program.\n";
+        exit(1);
+    }
+}
+
+int DayOfYear::getMonth()
+{
+    return month;
+}
+
+int DayOfYear::getDay()
+{
+    return day;
+}
