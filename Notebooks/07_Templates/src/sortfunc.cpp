5fb5c23581fece16751e8f81a8cd848b3f456a79
Alireza <128593284+Alireza-Khadem@users.noreply.github.com>
2024-02-29 12:49:30 +0330

    lecture 7 added

---
+++ Notebooks/07_Templates/src/sortfunc.cpp
@@ -0,0 +1,40 @@
+// This is file sortfunc.cpp
+template <class T>
+void swapValues(T &variable1, T &variable2)
+{
+    T temp;
+
+    temp = variable1;
+    variable1 = variable2;
+    variable2 = temp;
+}
+template <class BaseType>
+int indexOfSmallest(const BaseType a[], int startIndex, int numberUsed)
+{
+    BaseType min = a[startIndex];
+    int indexOfMin = startIndex;
+
+    for (int index = startIndex + 1; index < numberUsed; index++)
+        if (a[index] < min)
+        {
+            min = a[index];
+            indexOfMin = index;
+            // min is the smallest of a[startIndex] through a[index]
+        }
+
+    return indexOfMin;
+}
+
+template <class BaseType>
+void sort(BaseType a[], int numberUsed)
+{
+    int indexOfNextSmallest;
+    for (int index = 0; index < numberUsed - 1; index++)
+    { // Place the correct value in a[index]:
+        indexOfNextSmallest =
+            indexOfSmallest(a, index, numberUsed);
+        swapValues(a[index], a[indexOfNextSmallest]);
+        // a[0] <= a[1] <=…<= a[index] are the smallest of the original array
+        // elements. The rest of the elements are in the remaining positions.
+    }
+}
